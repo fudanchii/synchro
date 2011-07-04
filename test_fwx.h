32eac61425708e5c05efddbcd2b63c440aa9a561
Nurahmadie <nurahmadie@gmail.com>
2011-07-04 10:39:16 +0700

    add license for the lulz

--- test_fwx.h
+++ test_fwx.h
@@ -1,3 +1,20 @@
+/*
+ * synchro Test Suite Macro collection
+ * synchro is collection of simple macro
+ * I used to test my C/C++ unit.
+ * The design itself is dead simple and
+ * straight-forward, since I found another
+ * Unit Test tool for C/C++ is either bloated or 
+ * simply not enough for my need.
+ * If you read this source and has intention to use it,
+ * go on, I hereby permit the use of this source code
+ * under MIT/X11 license.
+ *
+ * Use it for good, not evil ;)
+ *
+ * Nurahmadie. 2011
+ * */
+
 #ifndef _TEST_FWX_H
 #define _TEST_FWX_H
 
